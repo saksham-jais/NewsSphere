7ddc10a8f56901e7bccb8ca032b6911fd21a5ab4
Saksham Jaiswal <sakshamjais100@gmail.com>
2025-06-02 16:50:03 +0530

    updated

--- news-backend/seed.js
+++ news-backend/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Post = require("./models/Post");
+const Post = require("./models/post");
 
 async function seedDB() {
   try {
--- news-backend/server.js
+++ news-backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Post = require("./models/Post"); // make sure this path is correct
+const Post = require("./models/post"); // make sure this path is correct
 
 const app = express();
 const PORT = process.env.PORT || 5000;
